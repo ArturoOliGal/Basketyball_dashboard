97d883328f6c129614279d4995f962d6f91b6c54
ArturoOliGal <arturoolivaresgalllegos@gmail.com>
2025-02-11 20:47:45 -0600

    Error

--- Basketball.py
+++ Basketball.py
@@ -26,7 +26,7 @@ def load_data(year):
 playerstats=load_data(selected_year)
 
 
-sorted_unique_team=sorted(playerstats.Tm.unique())
+sorted_unique_team=sorted(playerstats.unique())
 selected_team=st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
 
 unique_pos=['C','PF','SF','PG','SG']
